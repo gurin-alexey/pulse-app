b40d72451bef664595204f92ee6be1551d0ca30f
Alexey <2150814@gmail.com>
2026-01-04 17:13:05 +0200

    Fix: Make auth redirect URL dynamic for Vercel

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -38,6 +38,7 @@ export function Login() {
                     appearance={{ theme: ThemeSupa }}
                     providers={[]}
                     theme="light"
+                    redirectTo={`${window.location.origin}`}
                 />
             </div>
         </div>
