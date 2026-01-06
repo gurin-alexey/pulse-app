cba6f6425a4964b5fff5c528a59b44dfbe4be5ed
Alexey <2150814@gmail.com>
2026-01-06 19:53:11 +0200

    feat: enable PWA functionality with custom icons and offline support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,44 @@
-import { defineConfig } from 'vite' // Reload
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', 'logo.png'],
+      manifest: {
+        name: 'Pulse Productivity',
+        short_name: 'Pulse',
+        description: 'Mange your tasks with zero-latency',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
